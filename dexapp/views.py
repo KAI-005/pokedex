ceb55233dfa92e1648b75fec33d4baa0196f0308
Shreyansh V <shreyansh6410@gmail.com>
2025-06-01 13:03:58 +0530

    Fixed the sp atk sp def bug

--- dexapp/views.py
+++ dexapp/views.py
@@ -11,17 +11,21 @@ def search(request):
         response=requests.get(url)
         if response.status_code==200:
             data=response.json()
-            pokemon_data={
-                'name':data['name'].capitalize,
-                'image':data['sprites']['front_default'],
-                'types':[t['type']['name'] for t in data['types']],
-                'stats':{s['stat']['name']: s['base_stat'] for s in data['stats']},
-                'abilities':[a['ability']['name'] for a in data['abilities']],
-
-            }
+            pokemon_data = {
+    'name': data['name'].capitalize(),
+    'image': data['sprites']['front_default'],
+    'types': [t['type']['name'] for t in data['types']],
+    'abilities': [a['ability']['name'] for a in data['abilities']],
+    'hp': data['stats'][0]['base_stat'],
+    'attack': data['stats'][1]['base_stat'],
+    'defense': data['stats'][2]['base_stat'],
+    'special_attack': data['stats'][3]['base_stat'],
+    'special_defense': data['stats'][4]['base_stat'],
+    'speed': data['stats'][5]['base_stat'],
+}
         else:
             pokemon_data={'error':'Pokemon not found :( Try again!'}
 
-    return render(request, 'dexapp/pokedex.html',{'pokemon':pokemon_data})
+    return render(request, 'dexapp/pokedex.html',{"pokemon":pokemon_data})
 
 
